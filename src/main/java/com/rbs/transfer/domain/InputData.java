ec0d99078d1b9d9ba678dd958b1bf2fc1a2e6435
Bayram Ozinal <bayramozinal@gmail.com>
2019-06-16 19:44:22 +0100

    Add validation annotations to InputData class

--- src/main/java/com/rbs/transfer/domain/InputData.java
+++ src/main/java/com/rbs/transfer/domain/InputData.java
@@ -1,10 +1,21 @@
 package com.rbs.transfer.domain;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public class InputData {
+
+    @NotEmpty
+    @Size(min = 8, max = 10)
     private String sourceAccountNo;
+
+    @NotEmpty
+    @Size(min = 8, max = 10)
     private String destinationAccountNo;
+
+    @NotNull
     private BigDecimal amount;
 
     public String getSourceAccountNo() {
