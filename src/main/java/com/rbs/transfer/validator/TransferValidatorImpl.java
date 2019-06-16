be5e8ccf4a34e8639b5670d58d4116a295fcccf1
Bayram Ozinal <bayramozinal@gmail.com>
2019-06-16 20:06:33 +0100

    Add validation for source and destination accounts.

--- src/main/java/com/rbs/transfer/validator/TransferValidatorImpl.java
+++ src/main/java/com/rbs/transfer/validator/TransferValidatorImpl.java
@@ -1,5 +1,7 @@
 package com.rbs.transfer.validator;
 
+import com.rbs.transfer.domain.Account;
+
 import java.math.BigDecimal;
 
 public class TransferValidatorImpl implements TransferValidator {
@@ -10,6 +12,23 @@ public class TransferValidatorImpl implements TransferValidator {
         }
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new Exception("Amount should be bigger then zero.");
+        }
+    }
+
+    @Override
+    public void validateSourceAccount(Account sourceAccount, BigDecimal amount) throws Exception {
+        if (sourceAccount == null) {
+            throw new Exception("Source account not found.");
+        }
+        if (sourceAccount.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Source account does not has enough balance.");
+        }
+    }
+
+    @Override
+    public void validateDestinationAccount(Account destinationAccount) throws Exception {
+        if (destinationAccount == null) {
+            throw new Exception("Destination account is not found.");
         }
     }
 }
--- src/test/java/com/rbs/transfer/validator/TransferValidatorImplTest.java
+++ src/test/java/com/rbs/transfer/validator/TransferValidatorImplTest.java
@@ -1,5 +1,6 @@
 package com.rbs.transfer.validator;
 
+import com.rbs.transfer.domain.Account;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +16,9 @@ public class TransferValidatorImplTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
+
+    @Mock
+    private Account mockedAccount;
 
     @InjectMocks
     private TransferValidatorImpl validator;
@@ -32,5 +36,35 @@ public class TransferValidatorImplTest {
     @Test
     public void validateAmount_should_validate_amount_when_valid_parameter_passed() throws Exception {
         this.validator.validateAmount(BigDecimal.ONE);
+    }
+
+
+    @Test(expected = Exception.class)
+    public void validateSourceAccount_should_throw_Exception_when_account_does_not_exits() throws Exception {
+        this.validator.validateSourceAccount(null, BigDecimal.ONE);
+    }
+
+    @Test(expected = Exception.class)
+    public void validateSourceAccount_should_throw_Exception_when_there_is_no_enough_balance() throws Exception {
+        Mockito.when(this.mockedAccount.getBalance()).thenReturn(BigDecimal.ONE);
+        this.validator.validateSourceAccount(this.mockedAccount, BigDecimal.TEN);
+    }
+
+    @Test
+    public void validateSourceAccount_should_validate_account_balance() throws Exception {
+        Mockito.when(this.mockedAccount.getBalance()).thenReturn(BigDecimal.TEN);
+        this.validator.validateSourceAccount(this.mockedAccount, BigDecimal.ONE);
+    }
+
+    @Test(expected = Exception.class)
+    public void validateDestinationAccount_should_throw_AccountNotFoundException_when_account_does_not_exists() throws Exception {
+        this.validator.validateDestinationAccount(null);
+    }
+
+    @Test
+    public void validateDestinationAccount_should_validate_account() throws Exception {
+        Mockito.when(this.mockedAccount.getAccountNo()).thenReturn("11220938");
+        Mockito.when(this.mockedAccount.getBalance()).thenReturn(BigDecimal.TEN);
+        this.validator.validateDestinationAccount(this.mockedAccount);
     }
 }
